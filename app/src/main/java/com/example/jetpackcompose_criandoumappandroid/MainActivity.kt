480ad15b865a8b161290df8ab09fc026085c121b
Matheus Finamor Correa <matheusfinamor1@gmail.com>
2023-06-15 13:08:39 -0300

    Personalizando o design do app

--- app/src/main/java/com/example/jetpackcompose_criandoumappandroid/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcompose_criandoumappandroid/MainActivity.kt
@@ -4,18 +4,36 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcompose_criandoumappandroid.ui.theme.JetpackCompose_CriandoUmAppAndroidTheme
+import com.example.jetpackcompose_criandoumappandroid.ui.theme.Purple500
+import com.example.jetpackcompose_criandoumappandroid.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,17 +51,59 @@ class MainActivity : ComponentActivity() {
 @Preview(showBackground = true)
 @Composable
 fun ProductItem() {
-    Column {
-        Box(
+    Surface(
+        modifier = Modifier.padding(8.dp),
+        shape = RoundedCornerShape(15.dp),
+        tonalElevation = 4.dp
+    ) {
+        Column(
             modifier = Modifier
-                .height(50.dp)
-                .width(50.dp)
-        )
-        Image(
-            painter = painterResource(id = R.drawable.ic_launcher_background),
-            contentDescription = "Imagem do produto"
-        )
-        Text(text = "Texto 1")
-        Text(text = "Texto 2")
+                .heightIn(min = 250.dp, max = 300.dp)
+                .width(200.dp)
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .height(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500, Teal200
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "Imagem do produto",
+                    modifier = Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize/2)
+                        .clip(shape = CircleShape)
+                        .align(BottomCenter)
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .height(imageSize/2)
+            )
+            Column(modifier = Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis
+                )
+                Text(
+                    text = "R$ 14,99",
+                    modifier = Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400)
+                )
+            }
+        }
+
     }
 }
--- app/src/main/java/com/example/jetpackcompose_criandoumappandroid/ui/components/TestComponents.kt
+++ app/src/main/java/com/example/jetpackcompose_criandoumappandroid/ui/components/TestComponents.kt
@@ -1,23 +1,43 @@
 package com.example.jetpackcompose_criandoumappandroid.ui.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterStart
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
-import com.example.jetpackcompose_criandoumappandroid.ui.theme.JetpackCompose_CriandoUmAppAndroidTheme
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcompose_criandoumappandroid.R
+import com.example.jetpackcompose_criandoumappandroid.ui.theme.Purple40
+import com.example.jetpackcompose_criandoumappandroid.ui.theme.Purple500
+import com.example.jetpackcompose_criandoumappandroid.ui.theme.Teal200
 
+/*
 @Composable
 fun MyFirstComposable() {
     Text(text = "Meu primeiro texto!")
@@ -104,7 +124,7 @@ fun CustomLayoutPreview() {
         }
     }
 }
-
+**/
 @Preview(showBackground = true)
 @Composable
 fun DesafioImplDesignPreview() {
@@ -136,14 +156,86 @@ fun DesafioImplDesignPreview() {
 
 }
 
+@Preview(showBackground = true)
+@Composable
+fun DesignComTecnicasSimilares() {
+    Surface(
+        modifier = Modifier
+            .padding(8.dp),
+        shape = RoundedCornerShape(8.dp),
+        tonalElevation = 4.dp
+    ) {
+        Row(
+            modifier = Modifier
+                .height(200.dp)
+                .fillMaxWidth()
+        ) {
+            val imageSize = 100.dp
+            Box(
+                modifier = Modifier
+                    .width(imageSize)
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            listOf(
+                                Purple500,
+                                Teal200
+                            )
+                        )
+                    )
+                    .fillMaxHeight()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(imageSize)
+                        .align(CenterStart)
+                        .offset(x = imageSize / 2)
+                        .clip(shape = CircleShape)
+                        .border(
+                            BorderStroke(
+                                2.dp, brush = Brush.verticalGradient(
+                                    listOf(
+                                        Purple500, Purple40
+                                    )
+                                )
+                            ), CircleShape
+                        )
 
+
+                )
+            }
+            Spacer(
+                modifier = Modifier
+                    .width(imageSize/2)
+            )
+            Box(
+                modifier = Modifier
+                    .padding(32.dp)
+                    .fillMaxHeight()
+                    .align(CenterVertically)
+            ) {
+                Column {
+                    Text(
+                        text = LoremIpsum(50).values.first(),
+                        overflow = TextOverflow.Ellipsis,
+                        lineHeight = 20.sp
+                    )
+                }
+            }
+        }
+
+    }
+}
+
+/*
 @Preview(showBackground = true)
 @Composable
 fun MyFirstComposablePreview() {
     JetpackCompose_CriandoUmAppAndroidTheme {
         Surface {
-            MyFirstComposable()
+            DesignComTecnicasSimilares()
         }
     }
 }
-
+**/
